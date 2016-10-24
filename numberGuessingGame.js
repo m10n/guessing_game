f8eddf1c3205f9bd696c4c9b8e56ef8fe2c8e880
Morgan Missen <morganmissen@gmail.com>
2016-10-23 22:41:44 -0700

    numberGuessingGame solution

--- numberGuessingGame.js
+++ numberGuessingGame.js
@@ -1,23 +1,33 @@
-function NumberGuessingGame() {
+
+/*
+Write a **function** that is a number guessing game. It should determine a random number from 1-100, and let the user guess the number. It should report back whether their guess is correct, too high, or too low. Important: if the user guesses a number they've already guessed, it should tell them they have previously guessed this number. Until they guess the correct number, it should continue to prompt them. Once they've guessed the correct number, it should congratulate them and tell them how many guesses they took to solve the problem (minus any duplicate guesses). Thanks for playing!
+
+Use this link to begin playing:
+https://jsfiddle.net/m10n/cj2h3zh6/4/
+
+
+NB: this is just a single JavaScript function, not an app or website, so that fiddle.jshell.net pop-up alert *is* the game. Must have JavaScript/alerts enabled to play.
+
+*/
+
+function numberGuessingGame(){
   var number = Math.floor((Math.random() * 100) + 1);
   var guesses = [];
 
-  do {
-    var guess = Number(prompt("I'm thinking of a number between 1 and 100. Try to guess my number below."));
-    if (guesses.indexOf(guess) !== -1) {
+  while(guess !== number){
+    var guess = number(prompt("I'm thinking of a number between 1 and 100. Try to guess my number below."));
+    if (guesses.indexOf(guess) !== -1){
       alert("Oops, you already guessed " + guess + ". Guess a new number.");
     } else {
       guesses.push(guess);
       if (guess > number) {
         alert("Sorry, " + guess + " is too high! Guess a lower number.");
-      }
-      else if (guess < number) {
+      } else if(guess < number){
         alert("Sorry, " + guess + " is too low! Guess a higher number.");
       }
     }
-  } while (guess !== number);
-
+  }
   alert("Correct! My number was " + number + ". You guessed it in " + guesses.length + " tries.");
 }
 
-NumberGuessingGame();
+numberGuessingGame();
